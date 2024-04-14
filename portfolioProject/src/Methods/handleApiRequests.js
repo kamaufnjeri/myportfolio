1dbc5f698790d11676f0ca0d62b19ada3727bacf
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-04-14 18:10:21 +0300

    changed url for the deployed apps

--- portfolioProject/src/Methods/handleApiRequests.js
+++ portfolioProject/src/Methods/handleApiRequests.js
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 // handling posts requests
 export const handlePostRequests = async (url, data) => {
-  const urlRequest = `http://localhost:3000/${url}`;
+  const urlRequest = `https://myportfolio-y9w4.onrender.com/${url}`;
 
   return await axios.post(urlRequest, data)
     .then(response => {
@@ -32,7 +32,7 @@ export const handleMultiPartPostRequest = async (url, formData) => {
 
 // handleget requests
 export const handleGetRequests = async (url) => {
-    const urlRequest = `http://localhost:3000/${url}`;
+    const urlRequest = `https://myportfolio-y9w4.onrender.com/${url}`;
 
     return await axios.get(urlRequest)
     .then(response => response)
@@ -41,7 +41,7 @@ export const handleGetRequests = async (url) => {
 
 // deleting items
 export const handleDeleteRequests = async (url) => {
-    const urlRequest = `http://localhost:3000/${url}`;
+    const urlRequest = `https://myportfolio-y9w4.onrender.com/${url}`;
 
     return await axios.delete(urlRequest)
     .then(response => response)
@@ -50,7 +50,7 @@ export const handleDeleteRequests = async (url) => {
 
 // updating data
 export const handlePutRequests = async (url, data) => {
-    const urlRequest = `http://localhost:3000/${url}`;
+    const urlRequest = `https://myportfolio-y9w4.onrender.com/${url}`;
 
     return await axios.put(urlRequest, data)
     .then(response => response)
