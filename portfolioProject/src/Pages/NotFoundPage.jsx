961e647e805d2e95ffc0fcce15537b60e8ff589a
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-04-27 02:42:58 +0300

    made the not found responsive

--- portfolioProject/src/Pages/NotFoundPage.jsx
+++ portfolioProject/src/Pages/NotFoundPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import styles from "./NotFoundPage.module.css";
+import FindMe from "../Components/FindMe";
 
 export default function NotFoundPage() {
   return (
-    <div className={styles.notFound}>
-      <img src="/404error.png" alt="not found tag" />
+    <div>
+      <FindMe/>
+      <div className={styles.notFound}>
+        <img src="/404error.png" alt="not found tag" />
+      </div>
     </div>
+
   )
 }
