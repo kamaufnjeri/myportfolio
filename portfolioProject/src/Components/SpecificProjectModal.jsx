c6d83a23c88c9d1ffbe5b2f312e6a3296df48d37
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-09-30 22:16:02 +0300

    Handling closing of video when project modal is closed

--- portfolioProject/src/Components/SpecificProjectModal.jsx
+++ portfolioProject/src/Components/SpecificProjectModal.jsx
@@ -9,7 +9,7 @@ const SpecificProjectModal = ({ project, isOpen, onClose }) => {
   const projectRef = useRef(null);
   const [tools, setTools] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [iframeKey, setIframeKey] = useState(0);
+  const [videoSrc, setVideoSrc] = useState(project.videoUrl);
 
   const maxIndex = 3;
 
@@ -64,7 +64,7 @@ const SpecificProjectModal = ({ project, isOpen, onClose }) => {
       onClose();
       setCurrentIndex(0);
     }
-    setIframeKey((prevKey) => prevKey + 1);
+    setVideoSrc('');
   }
   return (
     <div className={styles.modalbox} ref={modalRef} onClick={closeModal}>
@@ -99,7 +99,9 @@ const SpecificProjectModal = ({ project, isOpen, onClose }) => {
           <div className={styles.mainBox}>
             <div className={styles.contentBox}>
               <h4>Project video :</h4>
-              {project.videoUrl && <div key={iframeKey}className={styles.videoBox} dangerouslySetInnerHTML={{ __html: project.videoUrl }}></div>}
+              {project.videoUrl && <div className={styles.videoBox}>
+              <iframe width="813" height="457" src={videoSrc} title="Project Demo Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>}
             </div>
           </div>
         
